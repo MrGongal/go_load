b38b68644bb4e21c6af65f61a55f28eb5ab2283a
MrGongal <140329673+MrGongal@users.noreply.github.com>
2024-04-17 21:54:07 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen 'src/index.html'
+
+files {
+    'src/index.html',
+    'src/logo/ex.mp3',
+    'src/logo/logo.png',
+    'src/logo/ex.mp4',
+}
